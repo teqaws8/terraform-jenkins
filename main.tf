4cd105002e15c8a18e943caa1d823c2df5438ae4
teqaws8 <109912617+teqaws8@users.noreply.github.com>
2022-11-02 12:18:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_instance" "webserver" {
 
   ami = "ami-02ea247e531eb3ce6"
   instance_type = var.instance_type
-  key_name = "EC2-keyPair-Name"
+  key_name = "transit-gw.pem"
  vpc_security_group_ids = [aws_security_group.web-sg.id]
 associate_public_ip_address = true
   root_block_device {
